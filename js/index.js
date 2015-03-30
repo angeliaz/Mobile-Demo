93919e86a9f7e78735e52fd8b4f828e589322483
Angelia <angeliaw@yeah.net>
2015-03-30 17:42:27 +0800

    add some custom settings

--- js/index.js
+++ js/index.js
@@ -4,14 +4,31 @@
 
 		init: function () {
 			
+			$('.bg').addClass('bg-move');
+			$('.word1').addClass('word1-move');
+			$('.word2').addClass('word2-move');
+			$('.word3').addClass('word3-move');
+
+			var height = $(win).height();
+			$('.content').height(height);
+
+			this.initParam();
+			this.eventBind();
+
 		},
 
-		// 判断方向
-		getDirection: function () {
+		initParam: function () {
+
+			this.currentPage = 1;
+			this.flag = false;
+			this.direction = 'up';
+			this.startX = this.startY = 0;
+			this.endX = this.endY = 0;
 
 		},
 
 		eventBind: function () {
+			
 			var _this = this;
 			var height = $(win).height();
 			var $scroll = $('.scrollContent');
@@ -19,45 +36,68 @@
 			var $word2 = $scroll.find('.word2');
 			var $word3 = $scroll.find('.word3');
 			var $bgPart = $scroll.find('.bg');
-			this.flag = false;
-			this.direction = 'up';
-			this.startX = this.startY = 0;
+			
+			
 
 			$('.wrap').on('touchstart', function (e) {
 
 				var touch = e.touches[0];
-				_this.flag = true;
-				_this.touchX = touch.pageX;
-				_this.touchY = touch.pageY;
+				
+				_this.startX = touch.pageX;
+				_this.startY = touch.pageY;
 
-				setTimeout(function () {
+				//setTimeout(function () {
 					$bgPart.removeClass('bg-move');
 					$word1.removeClass('word1-move');
 					$word2.removeClass('word2-move');
 					$word3.removeClass('word3-move');
-				}, 200);
+				//}, 200);
 
 			});
 
 			$('.wrap').on('touchmove', function (e) {
 
+				_this.flag = true;
+
+				var touch = e.touches[0];
+				_this.endX = touch.pageX;
+				_this.endY = touch.pageY;
+
 			});
 
 			$('.wrap').on('touchend', function (e) {
-				var top = parseInt($scroll.css("-webkit-transform").split(",")[1]);
-				console.log(top - height);
-				$scroll.css({
-							"-webkit-transform" : "translate3d(0px, " + (top - height) + "px, 0px);"});
-				$bgPart.addClass('bg-move');
-				$word1.addClass('word1-move');
-				$word2.addClass('word2-move');
-				$word3.addClass('word3-move');
+
+				if(_this.flag) {
+					var direction = (_this.endY - _this.startY <= 0) ? 'up' : 'down';
+					var top = parseInt($scroll.css("-webkit-transform").split(",")[1]);
+					
+
+					// 向上滑
+					if(direction === 'up') {
+						if(_this.currentPage < 4) {
+							$scroll.css({"-webkit-transform" : "translate3d(0px, " + (top - height) + "px, 0px);"});
+							_this.currentPage++;
+						}
+					} else {
+						if(_this.currentPage > 1) {
+							$scroll.css({"-webkit-transform" : "translate3d(0px, " + (top + height) + "px, 0px);"});
+							_this.currentPage--;
+						}
+					}
+
+
+					$bgPart.addClass('bg-move');
+					$word1.addClass('word1-move');
+					$word2.addClass('word2-move');
+					$word3.addClass('word3-move');
+				}
+				
 
 			});
 		}
 
 	};
 
-	Scroll.eventBind();
+	Scroll.init();
 
 })(window, document, Zepto);
